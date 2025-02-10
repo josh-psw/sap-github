eb85c1257a52bc7a55cb51d943fdbd24ae38b746
Josh <139473229+josh-psw@users.noreply.github.com>
2025-02-10 13:44:11 +0000

    Update run-scan.sh

--- run-scan.sh
+++ run-scan.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CREATE_SITE_BODY=$(cat <<EOF | jq -c
+CREATE_SITE_BODY=$(cat <<EOB | jq -c
 {
     "query": "mutation CreateSite(\$input: CreateSiteInput!) { create_site(input: \$input) { site { name id scope_v2 { start_urls in_scope_url_prefixes out_of_scope_url_prefixes protocol_options } } } }",
     "operationName": "CreateSite",
@@ -13,12 +13,12 @@ CREATE_SITE_BODY=$(cat <<EOF | jq -c
             },
             "parent_id": 0,
             "confirm_permission_to_scan": true,
-            "scan_configuration_ids": ["13467384-a8c8-49f9-8d45-68e70e3e8776"],
+            "scan_configuration_ids": ["8461d22c-c06a-4255-a6ef-bb5e56f0779a"],
             "application_logins": {}
         }
     }
 }
-EOF
+EOB
 )
 
 create_scan_body() {
@@ -39,7 +39,7 @@ create_scan_body() {
 scan_status_body() {
     local schedule_item_id="$1"
 
-    jq -nc --arg site_id "$site_id" '{
+    jq -nc --arg schedule_item_id "$schedule_item_id" '{
         "query": "query GetScan($schedule_item_id: ID) {scans(limit: 1, schedule_item_id: $schedule_item_id) { status } }",
         "operationName": "GetScan",
         "variables": {
@@ -67,11 +67,13 @@ make_request() {
     if [ "$response_code" -ne 200 ] || echo "$response_body" | grep -iq '"errors"'; then
         echo "$action_name: failed (code = $response_code)"
         echo "Response body: $response_body"
-        
+    
         return 1 
     else
         echo "$action_name request: success"
-                
+        echo "$body"
+        echo "$response_body"
+      
         if [ -n "$json_selector" ]; then
             extracted_value=$(echo "$response_body" | jq -r "$json_selector")
 
@@ -94,8 +96,8 @@ poll_scan_status() {
     local scan_status_body=$(scan_status_body "$SCHEDULE_ITEM_ID")
 
     while true; do
-        status=$(make_request "Check scan status" "$scan_status_body" ".data.scans[0].status" | tee /dev/tty | tail -n1)
-        
+        status=$(make_request "Check scan status" "$scan_status_body" ".data.scans[0].status" | tail -n1)
+
         echo "Current scan status: $status"
 
         if [ "$status" = "succeeded" ] || [ "$status" = "cancelled" ] || [ "$status" = "failed" ]; then
@@ -109,33 +111,30 @@ poll_scan_status() {
 
 # Create site
 echo "Creating site $SITE_NAME"
-SITE_ID=$(make_request "Create site" "$CREATE_SITE_BODY" ".data.create_site.site.id" | tee /dev/tty | tail -n1)
+SITE_ID=$(make_request "Create site" "$CREATE_SITE_BODY" ".data.create_site.site.id" | tail -n1)
 
 # Start scan
 echo "Starting scan"
 
 SCAN_BODY=$(create_scan_body "$SITE_ID")
-SCHEDULE_ITEM_ID=$(make_request "Start scan" "$SCAN_BODY" ".data.create_schedule_item.schedule_item.id" | tee /dev/tty | tail -n1)
+SCHEDULE_ITEM_ID=$(make_request "Start scan" "$SCAN_BODY" ".data.create_schedule_item.schedule_item.id" | tail -n1)
 
 # Poll for scan complete
 echo "Waiting for scan to complete"
-SCAN_STATUS=$(poll_scan_status $SCHEDULE_ITEM_ID)
+SCAN_STATUS=$(poll_scan_status $SCHEDULE_ITEM_ID | tail -n1)
 
-if [ -z "$scan_status" ]; then
+if [ -z "$SCAN_STATUS" ]; then
     echo "Something has gone wrong - scan status is empty. Exiting with -1"
     exit -1
-elif [ "$scan_status" = "failed" ]; then
+elif [ "$SCAN_STATUS" = "failed" ]; then
     echo "Scan failed. Exiting with -2"
     exit -2
-elif [ "$scan_status" = "cancelled" ]; then
+elif [ "$SCAN_STATUS" = "cancelled" ]; then
     echo "Scan was cancelled. Exiting with -3"
     exit -3
-elif [ "$scan_status" = "succeeded" ]; then
+elif [ "$SCAN_STATUS" = "succeeded" ]; then
     echo "Scan completed. View the results in the dashboard"
 else
-    echo "Unexpected scan status: $scan_status. Exiting with -4"
+    echo "Unexpected scan status: $SCAN_STATUS. Exiting with -4"
     exit -4
 fi
-
-
-# script can be modified to retrieve results after scan completed, show link to scan, have thresholds baased on vulnerabilities detected etc
